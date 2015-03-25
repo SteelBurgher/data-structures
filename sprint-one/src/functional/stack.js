38283360048c49b5fcab7dc0bc3780c5f4a2a7c8
SteelBurgher <brandon.patrick.ellis@gmail.com>
2015-03-25 18:49:59 -0400

    Made functional pattern stack pass

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,23 +1,24 @@
 var Stack = function(){
   var someInstance = {};
   var length = 0;
-  var last;
-  var prev;
+  
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
+  // { value: {value: value, prev: prev} }
+  // { length: value}
+
   someInstance.push = function(value){
-    storage[value] = value;
+    storage[length] = value;
     length ++;
-    last = storage[value];
   };
 
   someInstance.pop = function(){
     if(length > 0){
+      var foo = storage[length - 1];
+      delete storage[length - 1];
       length --;
-      var foo = last;
-      delete last;
       return foo;
     }
 
