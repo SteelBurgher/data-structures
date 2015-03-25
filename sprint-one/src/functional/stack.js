74a97fecf5c5de9d53c63e9f1770a194b0414f4c
SteelBurgher <brandon.patrick.ellis@gmail.com>
2015-03-25 16:13:42 -0400

    Nearly completed the stack data structure. Just working out the details of how the top member of the stack should always point to the member below it.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,30 @@
 var Stack = function(){
   var someInstance = {};
-
+  var length = 0;
+  var last;
+  var prev;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[value] = value;
+    length ++;
+    last = storage[value];
   };
 
   someInstance.pop = function(){
+    if(length > 0){
+      length --;
+      var foo = last;
+      delete last;
+      return foo;
+    }
+
   };
 
   someInstance.size = function(){
+     return length;
   };
 
   return someInstance;
