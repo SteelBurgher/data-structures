45db9b030c490bd23ef66512786816aa04ef7b44
SteelBurgher <brandon.patrick.ellis@gmail.com>
2015-03-25 20:15:02 -0400

    Made queue.js and stack.js pass in the style of functional-shared instantiation pattern.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,32 @@
 var Queue = function(){
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.length = 0;
+  obj.enqueued = 0;
+  obj.nowServing = 0;
+  obj.storage = {};
+  _.extend(obj, queueMethods);
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size : function() {  
+    return this.length;
+  },
+  enqueue: function(value){   
+    this.storage[this.enqueued] = value;
+    this.length ++;
+    this.enqueued ++;
+  },
+  dequeue: function() {
+    if(this.length > 0){
+      var temp = this.storage[this.nowServing];
+      delete this.storage[this.nowServing];
+      this.length --;
+      this.nowServing ++;
+      return temp;
+    }
+  }
+};
 
 
 
