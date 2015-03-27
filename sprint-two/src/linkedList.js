5a28c8b4df03b45be1bc171997404711e2045106
SteelBurgher <brandon.patrick.ellis@gmail.com>
2015-03-26 20:41:26 -0400

    Made all but the two last specs pass for the linked list implementation.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,9 +4,25 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newTail = Node(value);
+    if (this.tail !== null){
+      // write things here
+      // point new var to oldTail
+      var oldTail = this.tail;
+      oldTail.next = newTail;
+      this.tail = newTail;
+    } else {
+      this.tail = newTail;
+      this.head = newTail;
+    }
   };
 
   list.removeHead = function(){
+    // new head is what current head points to
+    // var newHead = this.head.next;
+    var oldHeadValue = this.head.value;
+    this.head = this.head.next;
+    return oldHeadValue;
   };
 
   list.contains = function(target){
@@ -23,6 +39,15 @@ var Node = function(value){
 
   return node;
 };
+/* scratch
+null > null
+// add val to tail
+{val: val1, next: null}
+// add 2nd val
+{val: val1, next: val2} > {val: val2, next: null}
+// remove head
+{val: val2, next: null}
+*/
 
 /*
  * Complexity: What is the time complexity of the above functions?
