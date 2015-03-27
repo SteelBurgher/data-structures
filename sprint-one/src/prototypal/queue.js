4e77cb4cd58c29e5dfe0de70a3c664332d174400
SteelBurgher <brandon.patrick.ellis@gmail.com>
2015-03-26 20:38:15 -0400

    Implemented a stack and queue in prototypal style. Also edited a RegEx bug in verifyClass.js

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.length     = 0;
+  obj.nowServing = 0;
+  obj.enqueued   = 0;
+  obj.storage    = {};
+  return obj;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value){
+  this.storage[this.enqueued] = value;
+  this.length ++;
+  this.enqueued ++;
+};
+queueMethods.dequeue = function(){
+  if(this.length > 0){
+    var temp = this.storage[this.nowServing];
+    delete this.storage[this.nowServing];
+    this.length --;
+    this.nowServing ++;
+    return temp;
+  }
+};
+queueMethods.size  = function(){
+  return this.length;
+};
+
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj.length = 0;
+  obj.storage = {};
+  return obj;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+  this.storage[this.length] = value;
+  this.length ++;
+};
+stackMethods.pop  = function(){
+  if (this.length > 0){
+    var temp = this.storage[this.length - 1];
+    delete this.storage[this.length - 1];
+    this.length --;
+    return temp;
+  }
+};
+stackMethods.size = function(){
+  return this.length;
+};
+
+
 
 
