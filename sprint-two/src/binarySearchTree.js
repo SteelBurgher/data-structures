1ec605b9c78390f177e570e603a44aa131179cb5
SteelBurgher <brandon.patrick.ellis@gmail.com>
2015-03-28 14:01:16 -0400

    Implemented binary search tree.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,80 @@
 var BinarySearchTree = function(value){
+  this.left  = null;
+  this.right = null;
+  this.value = value;
 };
 
+BinarySearchTree.prototype.insert = function(value) {
+  if(value === this.value){
+    return undefined;
+  }
+  if(value < this.value) {
+    if(this.left === null) {
+      this.left = new BinarySearchTree(value);
+    }
+    else{
+      this.left.insert(value);
+    }
+  }
+  if(value > this.value) {
+    if(this.right === null) {
+      this.right = new BinarySearchTree(value);
+    }
+    else{
+      this.right.insert(value);
+    }
+  }
+};
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+BinarySearchTree.prototype.contains = function(value){
+  if(value === this.value){
+    return true;
+  }
+  if(value < this.value) {
+    if(this.left === null) {      
+      return false;
+    }
+    else{
+      return this.left.contains(value);
+    }
+  }
+  else{
+    if(this.right === null) {
+      return false;
+    }
+    else {
+      return this.right.contains(value);
+    }
+  } 
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  cb(this.value);
+  if(this.left){
+    this.left.depthFirstLog(cb);
+  }
+  if(this.right){
+    this.right.depthFirstLog(cb);
+  }  
+  /*
+  start at root
+  cb root
+  if left 
+    cb root left
+  if right 
+    cb root right
+
+  */
+};
+
+// psuedocode for insert
+// start at root node
+  // compare to current node's value
+    // if greater
+      // check if current node's right prop points to a node
+        // if it does, compare node we are inserting to that node
+        // if it doesn't, assign current node's right prop to node we're inserting
+    // if less than
+        // check if current node's left prop points to a node
+        // if it does, compare node we are inserting to that node
+        // if it doesn't, assign current node's left prop to node we're inserting
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,36 +7,23 @@ var HashTable = function(){
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
   if(!this._storage.get(i)) {
-    // nothing at pos i yet
-    // this._storage.set(i, {k.toString(): v});
     this._storage.set(i, {});
     this._storage.get(i)[k.toString()] = v;
 
-    this._storage.each(function(x){
-      // console.log(x);
-
-    });
   }
   else {
     this._storage.get(i)[k.toString()] = v;
-    this._storage.each(function(x){
-      // console.log(x);
-
-    });
   }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  console.log(this._storage.get(i));
-
   return this._storage.get(i)[k.toString()];
 
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  // this._storage.set(i, {k.toString(): null});
   this._storage.get(i)[k.toString()] = null;
 };
 
